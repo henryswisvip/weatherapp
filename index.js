87a563495b5c1688c36b82b00bd94e24c67eda05
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-03-28 08:40:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ fetch(APIUrl)
             case data.solarRadiation < 50 && data.metric.precipRate > 50 && data.metric.precipRate < 100:
                 image.src = 'images/heavy rain .png';
                 break;
-            case data.solarRadiation < 50 && data.metric.precipRate < 10:
+            case data.solarRadiation < 50 && data.metric.precipRate < 10 && data.metric.precipRate < 10:
                 image.src = 'images/drizzle.png';
                 break;
             case data.solarRadiation < 50:
