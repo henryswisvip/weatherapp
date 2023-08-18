468a8e4456f72aa4bc830a539b08b5619b8b23b3
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-08-18 12:25:45 +0800

    Update index.js

--- ComputerTabletsInterface/index.js
+++ ComputerTabletsInterface/index.js
@@ -24,49 +24,60 @@ function fetchCurrentWeatherData() {
             const winddir = document.querySelector('.windd .winddir span');
             const windChill = document.querySelector('.windd .windChill span');
             switch (true) {
-
-                case data.solarRadiation < 50 && data.metric.precipRate < 20 && data.metric.precipRate > 0:
-                    image.src = 'images/drizzle.png';
-
-                case data.solarRadiation < 50 && data.metric.precipRate > 50 && data.metric.precipRate < 100:
-                    image.src = 'images/heavy rain .png';
-                    break;
                     
-                case data.solarRadiation < 50 && data.metric.precipRate > 20 && data.metric.precipRate < 50:
-                    image.src = 'images/rain.png';
-                    break;
-                case data.solarRadiation < 50 && data.metric.precipRate < 10 && data.metric.precipRate > 0:
-                    image.src = 'images/drizzle.png';
-                    break;
-                case data.solarRadiation < 50:
-                    image.src = 'images/cloudy.png';
-                    break;
-                case data.solarRadiation > 50 && data.solarRadiation < 100:
-                    image.src = 'images/partly cloudy.png';
-                    break;
-                case data.solarRadiation > 100:
-                    image.src = 'images/clear.png';
-                    break;
-                case data.metric.temp <= 5:
-                    image.src = 'images/snow.png';
-                    break;
-                case data.metric.temp > 5 && data.metric.temp <= 15:
-                    image.src = 'images/cloud.png';
-                    break;
-                case data.metric.temp > 15 && data.metric.temp <= 25:
-                    image.src = 'images/clear.png';
-                    break;
-                case data.metric.temp > 25:
-                    image.src = 'images/hot.png';
-                    break;
-                case data.solarRadiation == 0:
-                    image.src = 'images/moon.png';
-                    break;
+    case data.solarRadiation >= 50 && data.metric.precipRate >= 20 && data.metric.precipRate <= 50:
+        image.src = 'images/drizzle.png';
+        break;
 
-                default:
-                    image.src = 'images/mist.png';
-                    break;
-            }
+    case data.solarRadiation >= 50 && data.metric.precipRate <= 50 && data.metric.precipRate >= 100:
+        image.src = 'images/heavy rain .png';
+        break;
+
+    case data.solarRadiation >= 50 && data.metric.precipRate <= 20 && data.metric.precipRate >= 50:
+        image.src = 'images/rain.png';
+        break;
+
+    case data.solarRadiation >= 50 && data.metric.precipRate >= 10 && data.metric.precipRate <= 0:
+        image.src = 'images/drizzle.png';
+        break;
+
+    case data.solarRadiation >= 50:
+        image.src = 'images/cloudy.png';
+        break;
+
+    case data.solarRadiation <= 50 && data.solarRadiation >= 100:
+        image.src = 'images/partly cloudy.png';
+        break;
+
+    case data.solarRadiation <= 100:
+        image.src = 'images/clear.png';
+        break;
+
+    case data.metric.temp > 5:
+        image.src = 'images/snow.png';
+        break;
+
+    case data.metric.temp <= 5 && data.metric.temp > 15:
+        image.src = 'images/cloud.png';
+        break;
+
+    case data.metric.temp <= 15 && data.metric.temp > 25:
+        image.src = 'images/clear.png';
+        break;
+
+    case data.metric.temp <= 25:
+        image.src = 'images/hot.png';
+        break;
+
+    case data.solarRadiation != 0:
+        image.src = 'images/moon.png';
+        break;
+
+    default:
+        image.src = 'images/mist.png';
+        break;
+}
+
 
         
             
