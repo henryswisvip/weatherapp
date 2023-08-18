7db4dbb7a07a60acf732280a2cc906a23489fc3e
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-08-18 18:35:21 +0800

    Update index.js

--- ComputerTabletsInterface/index.js
+++ ComputerTabletsInterface/index.js
@@ -27,17 +27,13 @@ function fetchCurrentWeatherData() {
                 case data.solarRadiation == 0:
                     image.src = 'images/moon.png';
                     break;
-                case data.solarRadiation > 50 && data.metric.precipRate < 10 && data.metric.precipRate > 0:
-                    image.src = 'images/drizzle.png';
-                    break;
                 case data.solarRadiation < 50 && data.metric.precipRate > 50 && data.metric.precipRate < 100:
                     image.src = 'images/heavy rain.png';
                     break;
-                    
                 case data.solarRadiation < 50 && data.metric.precipRate > 10 && data.metric.precipRate < 50:
                     image.src = 'images/rain.png';
                     break;
-                case data.solarRadiation < 50 && data.metric.precipRate < 10 && data.metric.precipRate > 0:
+                case data.solarRadiation > 50 && data.metric.precipRate < 10 && data.metric.precipRate > 0:
                     image.src = 'images/drizzle.png';
                     break;
                 case data.solarRadiation < 50:
@@ -46,21 +42,27 @@ function fetchCurrentWeatherData() {
                 case data.solarRadiation > 50 && data.solarRadiation < 100:
                     image.src = 'images/partly cloudy.png';
                     break;
-                case data.solarRadiation > 100:
-                    image.src = 'images/clear.png';
-                    break;
-                case data.metric.temp <= 5:
-                    image.src = 'images/snow.png';
-                    break;
                 case data.metric.temp > 5 && data.metric.temp <= 15:
                     image.src = 'images/cloud.png';
                     break;
-                case data.metric.temp > 15 && data.metric.temp <= 25:
+                case data.solarRadiation > 100:
                     image.src = 'images/clear.png';
                     break;
                 case data.metric.temp > 25:
                     image.src = 'images/hot.png';
                     break;
+
+
+
+
+                case data.metric.temp <= 5:
+                    image.src = 'images/snow.png';
+                    break;
+
+                case data.metric.temp > 15 && data.metric.temp <= 25:
+                    image.src = 'images/clear.png';
+                    break;
+
 
                 default:
                     image.src = 'images/mist.png';
