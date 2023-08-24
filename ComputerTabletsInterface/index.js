ba05f977fb2f6493cc91b7924c9d20cc96ca1f64
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-08-24 15:06:41 +0800

    Update index.js

--- ComputerTabletsInterface/index.js
+++ ComputerTabletsInterface/index.js
@@ -100,8 +100,8 @@ function fetchCurrentWeatherData() {
 
         
             
-            temperature.innerHTML = `${data.metric.heatIndex}<span>°C</span>`;
-            description.innerHTML = `Feels Like: ${data.metric.temp}`;
+            temperature.innerHTML = `${data.metric.temp}<span>°C</span>`;
+            description.innerHTML = `Feels Like: ${data.metric.heatIndex}`;
             humidity.innerHTML = `${data.humidity}%`;
             wind.innerHTML = `${data.metric.windSpeed} km/h`;
             solarRadiation.innerHTML = `${data.solarRadiation} kWh`;
