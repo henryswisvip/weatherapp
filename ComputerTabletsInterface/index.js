9bdfea0b130b8532b4c99a67c252b88a5e11a97a
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-08-24 12:15:19 +0800

    Update index.js

--- ComputerTabletsInterface/index.js
+++ ComputerTabletsInterface/index.js
@@ -53,7 +53,7 @@ function fetchCurrentWeatherData() {
                 case data.metric.precipRate > 50 && data.metric.precipRate < 100:
                     image.src = 'images/heavy rain .png';
                     break;
-                case data.solarRadiation < 50 && data.metric.precipRate > 10 && data.metric.precipRate < 50:
+                case data.metric.precipRate > 10 && data.metric.precipRate < 50:
                     image.src = 'images/rain.png';
                     break;
                 case data.solarRadiation > 50 && data.metric.precipRate > 10 && data.metric.precipRate < 50:
