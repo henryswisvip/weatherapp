66bcf7291b085634df9b0a496f90e34c700432fc
henryswisvip <47855607+henryswisvip@users.noreply.github.com>
2023-08-24 23:51:38 +0800

    Update index.js

--- ComputerTabletsInterface/index.js
+++ ComputerTabletsInterface/index.js
@@ -42,7 +42,7 @@ function fetchCurrentWeatherData() {
             }
 
             const data = json.data;
-
+            const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
