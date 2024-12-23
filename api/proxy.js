cd18881b9084e78eb6ff181bea139571aa5e0aed
henry wong <47855607+henryswisvip@users.noreply.github.com>
2024-12-23 18:49:57 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzcL7HnCrqRDDf-VuA7jneMGlhBRdG1pD3APENm2saywFF16kTxfKHJZGPisC_jFo9Xuw/exec'; // Replace YOUR_SCRIPT_ID
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxmBAK_XcA0PGw3xR6Eh08_hNRrSYHmbadVyahfS4J7ZdOYt181yAIO8x86HOMNHcDgNA/exec'; // Replace YOUR_SCRIPT_ID
     
     try {
         // Forward the request to Google Apps Script
